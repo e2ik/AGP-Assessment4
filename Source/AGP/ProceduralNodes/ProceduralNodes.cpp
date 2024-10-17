18201a6ae07a306e346c8ee828ff85203cfe3863
e2ik <erik.jensen.lin@gmail.com>
2024-10-17 18:00:55 +1100

    Update ProceduralNodes.cpp

--- Source/AGP/ProceduralNodes/ProceduralNodes.cpp
+++ Source/AGP/ProceduralNodes/ProceduralNodes.cpp
@@ -33,7 +33,7 @@ void UProceduralNodes::FindAllGroundMeshes()
         // TODO: Square pieces ray cast up do not add locations that's under another mesh
         // TODO: Buildings raycast down from centre, if there's ground piece under, then add node in center
         // TODO: Door mesh racast down, if there's ground piece under, then add node in center
-        
+        // TODO: Bridge mesh, manually place according to its bounding box
         if (Actor && Actor->GetActorLabel().Contains("Ground")) {
             UStaticMeshComponent* MeshComponent = Actor->GetStaticMeshComponent();
             if (MeshComponent) {
