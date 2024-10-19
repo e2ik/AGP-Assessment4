89bee92d0cc8e01a7e737bf4734f9a0989004aa2
e2ik <erik.jensen.lin@gmail.com>
2024-10-19 18:19:09 +1100

    spawning player start working

---
+++ Source/AGP/GameMode/AIDirector.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "Tickable.h"
+#include "AGP/BehaviourTree/AIBehaviourHeaders.h"
+#include "GameFramework/Actor.h"
+#include "GameFramework/PlayerStart.h"
+#include "AIDirector.generated.h"
+
+
+class ANavigationNode;
+
+UCLASS()
+class AGP_API UAIDirector : public UTickableWorldSubsystem
+{
+	GENERATED_BODY()
+
+public:
+
+	UAIDirector();
+    virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+    virtual void Deinitialize() override;
+	virtual void Tick(float DeltaTime) override;
+	virtual TStatId GetStatId() const override;
+
+protected:
+
+	UPROPERTY(EditAnywhere)
+	bool bHasFoundNodes;
+
+	UPROPERTY(EditAnywhere)
+	float DesiredTickInterval = 5.0f;
+	UPROPERTY()
+	float LastTickTime = 0.0f;
+
+	UPROPERTY()
+	TArray<ANavigationNode*> Nodes;
+
+private:
+
+	void RunCustomTick();
+
+	void GetRandomNode();
+	void FindAllNodes();
+	void PlacePlayerStarts(const FVector& Location);
+	
+};
